649584157f9389f610b4d1ec88ea29a63a5d233a
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-09-25 16:13:03 +0300

    TRY 2_3

--- src/js/api.js
+++ src/js/api.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const BASE_URL = `https://pixabay.com/api/`;
+// const BASE_URL = `https://pixabay.com/api/`;
 const API_KEY = '39579949-ce46c8eabb2900040151a1ef2';
 
 async function serchingPhoto(searchParam, perPage, page) {
     try {
-   
     const response = await axios.get(`?key=${API_KEY}&q=${searchParam}
       &image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
         return response.data;
-        
   } catch (error) {
         console.log(error.message);
-        
     }; 
 };
 
